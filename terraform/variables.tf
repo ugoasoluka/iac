e31361dc498ce5daecce197e23aff0ef37d3f49e
Ugo Asoluka <uasoluka25@yahoo.com>
2025-03-01 08:11:58 -0800

    changed ami id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0a7a4e87939439934" // Replace with the latest AMI ID for your region
+  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
